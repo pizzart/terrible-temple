0ae6daf9bf325082cb46816f0eb85efba0059e25
PizzArt <46490176+PizzArt@users.noreply.github.com>
2021-06-13 02:03:52 +0300

    this is a lot

--- scripts/room_trigger.gd
+++ scripts/room_trigger.gd
@@ -1,15 +1,16 @@
 extends Area2D
 
 export var pillar_path: NodePath
-var player_entered: bool
+var player: Node2D
 onready var pillar = get_node(pillar_path)
 
 func _on_body_entered(body: Node2D):
 	if body.is_in_group("Player"):
-		player_entered = true
+		player = body
 
 	if body.is_in_group("Enemy"):
-		if player_entered:
+		if player:
+			player.checkpoint = position
 			$Tween.interpolate_property(pillar, "position", pillar.position, pillar.position + Vector2(0, 32), 0.2, Tween.TRANS_SINE, Tween.EASE_IN)
 			$Tween.start()
 			yield($Tween, "tween_all_completed")
